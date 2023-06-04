bf368dc4142f5295f9cffd85b1f3e5b25c4df195
Jess Alejo <tbki.jea@gmail.com>
2023-06-04 11:42:00 +0800

    Create User
    - rails g model User username:string:uniq email:string:uniq first_name last_name
    - rails db:create
    - rails db:migrate

---
+++ db/migrate/20230604033947_create_users.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rails g model User username:string:uniq email:string:uniq first_name last_name
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+  end
+end
