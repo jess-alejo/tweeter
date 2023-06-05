cd55c1d328f8c30afa23c01496049b5c853e8b28
madelane.pasana <madelane.pasana@cybersoftbpo.com>
2023-06-05 23:59:16 +0800

    TWEETER : User Page : Display Current Username in Page
    * added timestamp feature to every tweet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(email, size = 32)
+    email_hash = Digest::MD5.hexdigest(email.downcase.strip)
+    "https://www.gravatar.com/avatar/#{email_hash}?s=#{size}&d=identicon"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  def avatar_url
+    if avatar.attached?
+    avatar.variant(resize: "100x100").processed.url
+    else
+    gravatar_url(email)
+    end
+  end
 end
