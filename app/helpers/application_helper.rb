9bd593e5d189a7d3d627bdc10e3cbfa0fdd67b5a
Jess Alejo <jess.alejo@cybersoftbpo.com>
2023-06-06 14:09:40 +0800

    TW-4 : Fix User Avatar Attachment
    - install active storage
    - add gem image_processing
    - update image_tag src

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def gravatar_url(email, size = 32)
-    email_hash = Digest::MD5.hexdigest(email.downcase.strip)
-    "https://www.gravatar.com/avatar/#{email_hash}?s=#{size}&d=identicon"
-  end
 end
