48f59264d4f5d54e0cd427b65beb65a620081bda
gabriel.austria <gabriel.austria@cybersoftbpo.com>
2023-06-08 15:58:46 +0800

    TW-5 : Added Custom Error Page for Error 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, flash: { alert: exception.message }
   end
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    flash[:alert] = error.message
+    render "errors/404", status: 404, layout:'errors'
+  end
 end
