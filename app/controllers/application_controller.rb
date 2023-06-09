f14afd0aa5bcfae70692dd67f046b470bf43a6e9
Jess Alejo <jess.alejo@cybersoftbpo.com>
2023-06-09 13:06:05 +0800

    TW-5 : Fix Alert Message double-render issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, flash: { alert: exception.message }
   end
+
   rescue_from ActiveRecord::RecordNotFound do |error|
-    flash[:alert] = error.message
+    flash.now[:alert] = error.message
     render "errors/404", status: 404, layout:'errors'
   end
 end
